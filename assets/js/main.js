3fd021850415b086f81ebc2ec0707fed88e2e26e
three2wan <kronologi88@gmail.com>
2020-01-10 17:31:44 +0800

    Design button in works section

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,46 +205,50 @@
 
 })(jQuery);
 
-const TypeWriter = function(txtElement, words, wait =3000) {
-	this.txtElement = txtElement;
-	this.words = words;
-	this.txt = '';
-	this.wordIndex = 0;
-	this.wait = parseInt(wait, 10);
-	this.type();
-	this.isDeleting = false;
-}
-
-TypeWriter.prototype.type = function() {
-	const current = this.wordIndex % this.words.length;
-	const fullTxt = this.words[current];
-	
-	if (this.isDeleting) {
-		this.txt = fullTxt.substring(0, this.txt.length - 1);
-	} else {
-		this.txt = fullTxt.substring(0, this.txt.length + 1);
-	}
-
-	this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-	
-	let typeSpeed = 300;
-
-	if (this.isDeleting) {
-		typeSpeed /= 2;
-	}
-
-	if (!this.isDeleting && this.txt === fullTxt) {
-		typeSpeed = this.wait;
-		this.isDeleting = true;
-	} else if (this.isDeleting && this.txt === '') {
+// ES6 create class Typewriter animation
+class TypeWriter 
+{
+	constructor(txtElement, words, wait =3000) 
+	{
+		this.txtElement = txtElement;
+		this.words = words;
+		this.txt = '';
+		this.wordIndex = 0;
+		this.wait = parseInt(wait, 10);
+		this.type();
 		this.isDeleting = false;
-		this.wordIndex++;
-		typeSpeed = 500;
 	}
 
-	setTimeout(() => this.type(), typeSpeed)
-}
+	type() {
+		const current = this.wordIndex % this.words.length;
+		const fullTxt = this.words[current];
+		
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
 
+		this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+		
+		let typeSpeed = 300;
+
+		if (this.isDeleting) {
+			typeSpeed /= 2;
+		}
+
+		if (!this.isDeleting && this.txt === fullTxt) {
+			typeSpeed = this.wait;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			this.wordIndex++;
+			typeSpeed = 500;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	}
+}
 
 document.addEventListener('DOMContentLoaded', init);
 
